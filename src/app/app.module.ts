de5804d1e99eb7562eae893743decff8844e9d5f
Metin Goerken <m.goerken91@gmail.com>
2023-02-14 20:01:49 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnboardingWizzardComponent } from './onboarding-wizzard/onboarding-wizzard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//MaterialUI
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent, OnboardingWizzardComponent],
-  imports: [BrowserModule, AppRoutingModule, MatSlideToggleModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatStepperModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/onboarding-wizzard/onboarding-wizzard.component.ts
+++ src/app/onboarding-wizzard/onboarding-wizzard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding-wizzard',
   templateUrl: './onboarding-wizzard.component.html',
-  styleUrls: ['./onboarding-wizzard.component.scss']
+  styleUrls: ['./onboarding-wizzard.component.scss'],
 })
 export class OnboardingWizzardComponent implements OnInit {
+  wizzardForm = new FormGroup({
+    name: new FormControl(''),
+    hauptinstrument: new FormControl(''),
+    einschaetzungInstrument: new FormControl(''),
+    genre: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

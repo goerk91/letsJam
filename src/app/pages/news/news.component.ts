68b66b5b2a5384a166b1344d730d27fc1c2407cf
Metin Goerken <m.goerken91@gmail.com>
2023-04-17 12:33:03 +0200

    Local Storage: Storing Form Data

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from 'src/app/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-news',
@@ -9,6 +11,12 @@ export class NewsComponent {
   tabs: string[] = ['Relevanz', 'Für mich', 'Musikersuche', 'Nachrichten'];
   activatedTabIndex: number = 0;
 
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    localStorage.removeItem('wizzardData');
+    this.dialog.open(DialogComponent);
+  }
   tabChange(index: number) {
     this.activatedTabIndex = index;
   }

122bd3fb10381f952e5c94db7ca1c30d76088b07
Metin Goerken <m.goerken91@gmail.com>
2023-04-19 20:29:43 +0200

    fix

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent {
-
+  postContribution() {
+    console.log('beitrag veröffentlichen');
+  }
 }

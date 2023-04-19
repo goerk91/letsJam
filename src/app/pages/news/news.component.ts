053459e4b7b9ec3acb89c1ec5b78f59b5b4e5bbc
Metin Goerken <m.goerken91@gmail.com>
2023-04-19 15:18:14 +0200

    Card & Dialog-Komponente

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+
+//Material UI
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogComponent } from 'src/app/components/dialog/dialog.component';
 
 @Component({
@@ -11,11 +13,22 @@ export class NewsComponent {
   tabs: string[] = ['Relevanz', 'Für mich', 'Musikersuche', 'Nachrichten'];
   activatedTabIndex: number = 0;
 
+  longText =
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
+    //remove WizzardData:
+    // localStorage.removeItem('wizzardData')
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    // dialogConfig.autoFocus = true;
+
     localStorage.removeItem('wizzardData');
-    this.dialog.open(DialogComponent);
+    this.dialog.open(DialogComponent, dialogConfig);
   }
   tabChange(index: number) {
     this.activatedTabIndex = index;
